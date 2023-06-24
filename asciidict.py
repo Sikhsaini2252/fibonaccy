8acebeb81628c57b6bead41bda669e895068b7e1
Sikhsaini2252 <137095863+Sikhsaini2252@users.noreply.github.com>
2023-06-24 22:48:25 +0530

    Add files via upload

---
+++ asciidict.py
@@ -0,0 +1,17 @@
+asciidict = dict()
+
+for i in range(97,123):
+
+    asciidict[chr(i)]=i
+
+key=asciidict.keys()
+
+value=asciidict.values()
+
+print("ASCII Dict:", asciidict)
+
+n= str(input("enter the key=>"))
+
+print("key:", n)
+
+print("value:", asciidict[n])
