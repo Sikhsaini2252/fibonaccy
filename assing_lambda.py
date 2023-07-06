e7dce9e727954f15f962d72d422f3618edbad8a0
Sikhsaini2252 <137095863+Sikhsaini2252@users.noreply.github.com>
2023-07-06 21:11:39 +0530

    Update assing_lambda.py

--- assing_lambda.py
+++ assing_lambda.py
@@ -1,5 +1,5 @@
 ## Write a Python program to create a lambda function that
 ## adds 25 to a given number passed in as an argument.
-
+y = int(input("Enter your int value here : "))
 x=lambda a : a + 25
-print(x(10))
+print(x(y))
